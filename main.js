ca46bdff6f003b5fdf7b2aa81a6000f91b43ddda
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-02-28 13:39:55 -0500

    added split function

---
+++ main.js
@@ -0,0 +1,19 @@
+let sonnets = require('./sonnets.js');
+
+let puck = sonnets.split(" ");
+function wordFrequency("sonnets") {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+let testRunner = require('./word-frequency-tests.js');
+testRunner(wordFrequency);
