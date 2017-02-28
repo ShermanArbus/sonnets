27307632ddbb77d0b3dd0e74c5ae2c3d2c0aae88
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-02-28 14:59:12 -0500

    created function romeo to log each index in puck

--- main.js
+++ main.js
@@ -1,26 +1,35 @@
 let sonnets = require('./sonnets.js');
 
-let puck = sonnets.split(" ");
-
-
-console.log(puck);
-
 
 
 
 function wordFrequency(sonnets) {
-
-}
-
-
-
-
-
+  let puck = sonnets.split(" ");
+  let juliet = {};
+  puck.forEach( function romeo(each) {
+    console.log(each);
+  })
 
 
+  // console.log('my stuff', puck);
+
+  return juliet;
+};
+
+wordFrequency(sonnets);
 
 
 
 
-let testRunner = require('./word-frequency-tests.js');
-testRunner(wordFrequency);
+
+// juliet.forEach( {
+//   if juliet.includes(" ") {
+//     //do something to delete or ignore this index
+//   }
+//   else let wordCount = new Object();
+
+
+
+
+// let testRunner = require('./word-frequency-tests.js');
+// testRunner(wordFrequency);
