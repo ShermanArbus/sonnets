2cac374a847501ec1070f211e62d3adfae7a1739
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-02-28 16:29:29 -0500

    created fizzbuzz files, added for loop to fill cats array

---
+++ fizzbuzz.js
@@ -0,0 +1,35 @@
+function fizzbuzz(x) {
+  let cats = [];
+  for (var i = 0; i < x; i++) {
+     console.log(i);
+     // more statements
+  }
+};
+
+
+fizzbuzz(57);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+// let testRunner = require('./fizzbuzz-tests.js');
+// testRunner(fizzBuzz);
--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function wordFrequency(sonnets) {
   let puck = sonnets.split(" ");
   let juliet = {};
   puck.forEach(function romeo(eachWord) {
-    // console.log(eachWord);
     if (juliet[eachWord] >= 1) {
       juliet[eachWord] = juliet[eachWord] + 1;
     }
@@ -17,25 +16,14 @@ function wordFrequency(sonnets) {
   })
 
 
-  // console.log('my stuff', puck);
-
   return juliet;
 };
 
-let cats = wordFrequency('cats are awesome and are soft');
-console.log(cats);
+let macbeth = wordFrequency(sonnets);
+console.log(macbeth);
 
 
 
 
-// juliet.forEach( {
-//   if juliet.includes(" ") {
-//     //do something to delete or ignore this index
-//   }
-//   else let wordCount = new Object();
-
-
-
-
-// let testRunner = require('./word-frequency-tests.js');
-// testRunner(wordFrequency);
+let testRunner = require('./word-frequency-tests.js');
+testRunner(wordFrequency);
