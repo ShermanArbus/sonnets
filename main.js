2bb1f3e8daf81e3cee98a4cadb6a92e8f133dbe8
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-02-28 15:44:02 -0500

    added word frequency count

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ let sonnets = require('./sonnets.js');
 function wordFrequency(sonnets) {
   let puck = sonnets.split(" ");
   let juliet = {};
-  puck.forEach( function romeo(each) {
-    console.log(each);
+  puck.forEach(function romeo(eachWord) {
+    // console.log(eachWord);
+    if (juliet[eachWord] >= 1) {
+      juliet[eachWord] = juliet[eachWord] + 1;
+    }
+    else {
+    juliet[eachWord] = 1;
+    }
   })
 
 
@@ -16,8 +22,8 @@ function wordFrequency(sonnets) {
   return juliet;
 };
 
-wordFrequency(sonnets);
-
+let cats = wordFrequency('cats are awesome and are soft');
+console.log(cats);
 
 
 
